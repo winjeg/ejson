f87a5748e4e201a7e39f294c4564521f01490d6a
gongwenjie09147 <gongwenjie09147@hellobike.com>
2022-03-15 11:20:41 +0800

    fix json error can't load

--- popup.js
+++ popup.js
@@ -3,38 +3,29 @@ const rightViewer = document.getElementById('viewer')
 const STORE_KEY = 'easy-json-editor';
 
 function loadEditor() {
+    var json = {}
     try {
-        const viewerOptions = {
-            mode: 'view',
-        }
-        const editorOptions = {
-            mode: 'code',
-            modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
-            onModeChange: function (newMode, oldMode) {
-                console.log('Mode switched from', oldMode, 'to', newMode)
-            },
-            onChangeText: function (jsonString) {
-                localStorage.setItem(STORE_KEY, jsonString);
-                jsonViewer.updateText(jsonString)
-            }
-        }
-        const json = localStorage.getItem(STORE_KEY) ||  '{}' 
-        const jsonEditor = new JSONEditor(leftEditor, editorOptions, JSON.parse(json))
-        const jsonViewer = new JSONEditor(rightViewer, viewerOptions, JSON.parse(json))
-
+        const jsonStr = localStorage.getItem(STORE_KEY) || '{}'
+        json = JSON.parse(jsonStr)
     } catch (error) {
-        // console.log(error)
+        json = {}
     }
+    const viewerOptions = {
+        mode: 'view',
+    }
+    const editorOptions = {
+        mode: 'code',
+        modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+        onModeChange: function (newMode, oldMode) {
+            console.log('Mode switched from', oldMode, 'to', newMode)
+        },
+        onChangeText: function (jsonString) {
+            localStorage.setItem(STORE_KEY, jsonString);
+            jsonViewer.updateText(jsonString)
+        }
+    }
+    const jsonEditor = new JSONEditor(leftEditor, editorOptions, json)
+    const jsonViewer = new JSONEditor(rightViewer, viewerOptions, json)
 }
 
 document.addEventListener('DOMContentLoaded', loadEditor, false);
-
-
-chrome.tabs.executeScript(function() {
-    if (chrome.runtime.lastError) {
-       var errorMsg = chrome.runtime.lastError.message
-       if (errorMsg == "Cannot access a chrome:// URL") {
-           // Error handling here
-       }
-    }
-})
